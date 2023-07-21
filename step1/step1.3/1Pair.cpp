c1cdaf1dbc722a77b3beac8de5edb77a1e9bc235
Priyanshu Singh <in.priyanshusingh@gmail.com>
2023-07-21 21:04:23 +0530

    changes

--- step1/step1.3/1Pair.cpp
+++ step1/step1.3/1Pair.cpp
@@ -1,7 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+void explainPair() {
+    pair<int,string> p={1,"Priyanshu Singh"};
+    cout<<p.first<<' '<<p.second<<'\n';
 
+    pair<int,pair<char,string>> p2={2,{'A',"Hello Pair!"}};
+    cout<<p2.first<<' '<<p2.second.first<<' '<<p2.second.second<<'\n';
+    pair<int,int> arr[]={{2,3}, {23,4}, {342,4}, {43,2}};
+    cout<<arr[0].second;
+}
+
+int main() {
+    explainPair();
     return 0;
 }
