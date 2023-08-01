6f42a3284364680ae57a447375c880c56d3bb1ca
Priyanshu Singh <in.priyanshusingh@gmail.com>
2023-08-01 09:45:53 +0530

    minor

--- step1/step1.3/2VectorsContainer.cpp
+++ step1/step1.3/2VectorsContainer.cpp
@@ -1,1 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void explainVector(){
+    vector<int> v;
+    v.emplace_back(33);
+    v.emplace_back(3);
+    v.emplace_back(332);
+    cout<<v[0]<<' '<<v[1]<<' '<<v[2]<<'\n';
+}
+
+void explainPairVector(){
+    vector<pair<int,char>>pairVector;
+    pairVector.emplace_back(2,'a');
+    pairVector.emplace_back(4342,'r');
+    cout<<pairVector[1].second;
+}
+
+void explainVector1(){
+
+}
+
+int main(){
+    explainVector();
+    explainPairVector();
+    return 0;
+}
