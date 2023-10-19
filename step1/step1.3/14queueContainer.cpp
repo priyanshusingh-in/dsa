2e300bf06786ec2abf8581bb2ab29b7d7cd59d79
Priyanshu Singh <in.priyanshusingh@gmail.com>
2023-10-19 12:41:00 +0530

    new code file

--- step1/step1.3/14queueContainer.cpp
+++ step1/step1.3/14queueContainer.cpp
@@ -9,12 +9,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void displayQueue(queue<int>q)
+{
+    cout<<"The elements of the queue are:"<<endl;
+    queue<int>q2=q;
+    while(!q2.empty())
+    {
+        cout<<q2.front()<<"\n";
+        q2.pop();
+    }
+}
+
 int main()
 {
 
     queue<int>q;
     for(int i=1;i<=5;i++)
-        q.push()
+        q.push(i);
+    
+    displayQueue(q);
+
+    cout<<"The size of the queue: "<<q.size()<<endl;
+    cout<<"The front element of the queue: "<<q.front()<<endl;
+    cout<<"popping the front element:"<<endl;
+    q.pop();
+    displayQueue(q);
 
     return 0;
 }
