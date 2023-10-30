8cc3b2b1cd358aa04aab91a37aa0b17fe937a278
Priyanshu Singh <in.priyanshusingh@gmail.com>
2023-10-30 13:22:51 +0530

    new code file

--- step1/step1.3/29lower_Bound.cpp
+++ step1/step1.3/29lower_Bound.cpp
@@ -2,3 +2,21 @@
 //simila to binary search
 //logn
 //syntax(ind stores the index of the element): int ind=lower_bound(a.begin(),a.end(),elementToSearch)-a.begin();
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Q1.Find the first occurrence of a X in a sorted array.
+//if it does not exits, print -1
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int>a={1,4,4,4,4,9,9,10,11};
+    int x=11;
+    int ind=lower_bound(a.begin(),a.end(),x)-a.begin();
+    if(ind!=a.end()-a.begin()&&a[ind]==x)
+        cout<<ind;
+    else
+        cout<<-1;
+    return 0;
+}
