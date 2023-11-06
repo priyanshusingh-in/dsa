a7bed36b9ac0d22ad382eb0e67fd0905bdb47d11
Priyanshu Singh <in.priyanshusingh@gmail.com>
2023-11-06 19:20:19 +0530

    new file

---
+++ step1/step1.4/countDigits.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void countDigits(int n){
+	// Write your code here.
+	int count=0;
+	while(n>0)
+	{
+		n=n/10;
+		count++;
+	}
+	cout<<count;
+}
+
+int main()
+{
+    countDigits(43435);
+    return 0;
+}
