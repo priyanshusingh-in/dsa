37cfdda6eb1b9f5427953619b767dd27a838bfc3
Priyanshu Singh <in.priyanshusingh@gmail.com>
2023-12-21 21:59:22 +0530

    new file

---
+++ step1(Basics)/step1.5(Recursion)/5sumNNum.cpp
@@ -0,0 +1,22 @@
+//https://www.codingninjas.com/studio/problems/sum-of-first-n-numbers_8876068?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long sumFirstN(long long n) {
+    // Write your code here.
+    if(n <= 0)
+        return 0;
+    else
+        return n + sumFirstN(n-1);
+}
+
+int main() {
+
+    long long n;
+    cin >> n;
+
+    cout << sumFirstN(n);
+
+    return 0;
+}
